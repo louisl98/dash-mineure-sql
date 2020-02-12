c12e72b3778f6b045ce83a6f934971f7a1158bbd
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-02-12 22:33:49 +0100

    update

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ def getprovinces():
         
 
 # front-end
-provinces = getprovinces()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 labels = []
 values = []
+provinces = getprovinces()
 for i in range(len(provinces)): 
     labels.append(provinces[i][0])
     values.append(provinces[i][3])
