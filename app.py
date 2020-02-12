8d3af072a5a978b563e7d817f95b2b99fd1885f1
Louis <louis-laugier@hotmail.fr>
2020-02-12 16:43:10 +0100

    DB

--- app.py
+++ app.py
@@ -5,26 +5,23 @@ import dash_html_components as html
 import psycopg2
 
 # database
-def dbconnect():
+def getprovinces():
     try:
         connection = psycopg2.connect(host="ec2-46-137-177-160.eu-west-1.compute.amazonaws.com", database="dbntjps2ogiaoe", user="jqwpptjnjyvaqw", password="cb6d58e6c0a9e078767b32777fe990054d317a029116a89fd35a8a29e974dcfc")
-        # create a cursor
+        # declare a cursor
         cur = connection.cursor()
         # get cities
-        cities = cur.execute("SELECT * FROM corona")
-        rows = cur.fetchone()
-        while rows is not None:
-            print(rows)
-            rows = cur.fetchone()
-       # close the communication with the PostgreSQL
+        cur.execute("SELECT * FROM corona")
+        provinces = cur.fetchall()
+        print(provinces[0])
+        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if connection is not None:
             connection.close()
-            print('Database connection closed.')
-
+        
 
 # front-end
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -48,5 +45,5 @@ app.layout = html.Div(children=[
     )
 ])
 if __name__ == '__main__':
-    dbconnect()
+    getprovinces()
     app.run_server(debug=True)
