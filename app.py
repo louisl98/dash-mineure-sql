bbf95efbb04086328e1c0d25234239940fcc7772
Louis <louis-laugier@hotmail.fr>
2020-02-12 16:57:38 +0100

    graph

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def getprovinces():
         # get cities
         cur.execute("SELECT * FROM corona")
         provinces = cur.fetchall()
-        print(provinces[0])
         # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -21,9 +20,11 @@ def getprovinces():
     finally:
         if connection is not None:
             connection.close()
+    return provinces
         
 
 # front-end
+provinces = getprovinces()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(children=[
@@ -35,8 +36,8 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [provinces[0][0]], 'y': [provinces[0][3]], 'type': 'bar', 'name': provinces[0][0]}, # Hubei
+                {'x': [provinces[1][0]], 'y': [provinces[1][3]], 'type': 'bar', 'name': provinces[1][0]}, # Guangdong
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
